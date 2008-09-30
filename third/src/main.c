cacea7d7c0f3f35503a1c6a75b5583cf78d021d8
Julian O'Brien <jmobrien@luminapower.com>
2008-09-30 16:35:55 -0400

    Finished project 3

--- third/src/main.c
+++ third/src/main.c
@@ -8,35 +8,61 @@
 #include "stdio.h"
 #include "math.h"
 
-#define TWO											2.0
-#define THREE										3.0
-#define FOUR										4.0
+//
+// These are the two polynomial equations used
+//
 
-#define NEG_POINT_TWELVE 				(-0.12)
-#define TWELVE 									12.0
-#define THREE_EIGHTY						380.0
-#define FOURTY_ONE_HUNDRED 			4100.0
-#define TWO_TWENTY							220.0
+// This equation returns the altitude in meters of the balloon
+// at the given hour t.
+// a(t) = -0.12t^4 + 12t^3 - 380t^2 + 4100t + 220
+#define calculate_altitude(t) (\
+          ((-0.12) * pow(t, 4.0)) + \
+          (12.0 * pow(t, 3.0)) - \
+          (380.0 * pow(t, 2.0)) + \
+          (4100.0 * t) + 220.0 \
+        )
 
-#define NEG_POINT_FOURTY_EIGHT	(-0.48)
-#define THIRTY_SIX							36.0
-#define SEVEN_SIXTY							760.0
+// This equation returns the velocity in meters/hour of the balloon
+// at a given hour t.
+// v(t) = -0.48t^3 + 36t^2 - 760t + 4100
+#define calculate_velocity(t) (\
+          ((-0.48) * pow(t, 3.0)) + \
+          (36.0 * pow(t, 2.0)) - \
+          (760.0 * t) + 4100.0 \
+        )
 
-#define MAX_INPUT_VALUE					48
-#define SIXTY										60
+// used in the conversion
+#define MINUTES_IN_HOUR     60.0
+#define SECONDS_IN_MINUTE   60.0
 
-#define CELL_PADDING						10
-#define NEW_LINE								"\n"
+// the maxmimum input allowable for any of the inputs
+#define MAX_INPUT_VALUE     48.0
+
+// the maximum amount of time steps allowed
+#define MAX_NUMBER_OF_ITERATIONS 100
+
+// helper
+#define PRINT_NEW_LINE     printf("\n");
+
+// table layout
+#define CELL_PADDING        10
+#define COL_1_PADDING       8
+#define COL_2_PADDING       12
+#define COL_3_PADDING       14
+
+// readability
+#define TRUE                1
+#define FALSE               1
 
 // loops displaying the message and getting the input
 // until the input is valid
 void get_valid_user_input(const char *message, double *input){
-  while (1){
+  while (TRUE){
     printf(message);
     scanf("%lf", input);
     // if the input greater than 0 and
     // less than the max, then it is valid
-    if (*input >= 0 && *input <= MAX_INPUT_VALUE){
+    if (*input >= 0.0 && *input <= MAX_INPUT_VALUE){
       break;
     }
   }
@@ -44,42 +70,65 @@ void get_valid_user_input(const char *message, double *input){
 
 // get the user input
 void get_user_input(double *initial_hour, double *increment_hour, double *final_hour){
-  get_valid_user_input("Enter the initial value for the table in hours ", initial_hour);
-  get_valid_user_input("Enter the increment bewteen lines in hours     ", increment_hour);
-  get_valid_user_input("Enter the final value for the table in hours   ", final_hour);
+  while(TRUE){
+    get_valid_user_input("Enter the initial value for the table in hours ", initial_hour);
+    get_valid_user_input("Enter the increment bewteen lines in hours     ", increment_hour);
+    get_valid_user_input("Enter the final value for the table in hours   ", final_hour);
+
+    // make sure the number of results to be calculated is
+    // less than the maximum allowed and more than 0
+    unsigned int number_of_iterations = 0;
+    number_of_iterations = floor( (*final_hour - *initial_hour) / *increment_hour );
+    if (number_of_iterations > MAX_NUMBER_OF_ITERATIONS){
+      printf("Please enter numbers that would cause less than %d iterations.\n",
+        MAX_NUMBER_OF_ITERATIONS);
+    } else if (0 == number_of_iterations) {
+      printf("Please enter numbers that would cause more than 0 iterations.\n");
+    } else {
+      break;
+    }
+  }
 }
 
-// calculate the altitude
-double calculate_altitude(const double *time_in_hours){
-  return (NEG_POINT_TWELVE * pow(*time_in_hours, FOUR)) +
-    (TWELVE * pow(*time_in_hours, THREE)) - (THREE_EIGHTY * pow(*time_in_hours, TWO)) +
-    (FOURTY_ONE_HUNDRED * *time_in_hours) + TWO_TWENTY;
+// convert meters/hr to meters/s
+double convert_mph_to_mps(const double velocity_in_mph){
+  return velocity_in_mph / (MINUTES_IN_HOUR * SECONDS_IN_MINUTE);
 }
 
-double convert_mph_to_mps(const double *velocity_in_mph){
-  return *velocity_in_mph / (SIXTY * SIXTY);
-}
-
-
-// calculate the velocity
-double calculate_velocity(const double *time_in_hours){
-  double velocity_in_mph;
-  velocity_in_mph = (NEG_POINT_FOURTY_EIGHT * pow(*time_in_hours, THREE)) +
-    (THIRTY_SIX * pow(*time_in_hours, TWO)) - (SEVEN_SIXTY * *time_in_hours) +
-    FOURTY_ONE_HUNDRED;
-  return convert_mph_to_mps(&velocity_in_mph);
-}
+//
+// printing functions
+//
 
 // print a double
-void print_d(const double *d){
-  printf("%*.2f", CELL_PADDING, *d);
+void print_d(const double d, const int padding){
+  printf("%*.2f", padding, d);
 }
 
-// print a string
-void print_s(const char *str){
-  printf("   %-*s", CELL_PADDING - 3, str);
+void print_hour(const double d){
+  print_d(d, COL_1_PADDING);
 }
 
+void print_altitude(const double d){
+  print_d(d, COL_2_PADDING);
+}
+
+void print_velocity(const double d){
+  print_d(d, COL_3_PADDING);
+}
+
+void print_col_1_heading(const char *str){
+  printf("    %-*s", COL_1_PADDING, str);
+}
+
+void print_col_2_heading(const char *str){
+  printf("  %-*s", COL_2_PADDING, str);
+}
+
+void print_col_3_heading(const char *str){
+  printf("  %-*s", COL_3_PADDING, str);
+}
+
+// main
 int main(){
   double initial_hour;
   double increment_hour;
@@ -88,36 +137,42 @@ int main(){
   get_user_input(&initial_hour, &increment_hour, &final_hour);
 
   // space the table away from the input
-  printf(NEW_LINE); printf(NEW_LINE);
+  PRINT_NEW_LINE; PRINT_NEW_LINE;
 
   // print the table heading
-  printf("Weather Balloon Information"); printf(NEW_LINE);
+  printf("Weather Balloon Information"); PRINT_NEW_LINE;
 
-  print_s("Time");
-  print_s("Height");
-  print_s("Velocity");
-   printf(NEW_LINE);
+  // print the table headings
+  print_col_1_heading("Time");
+  print_col_2_heading("Height");
+  print_col_3_heading("Velocity");
+  PRINT_NEW_LINE;
 
-  print_s("hrs");
-  print_s("meters");
-  print_s("meters/s");
-  printf(NEW_LINE);
+  print_col_1_heading("hrs");
+  print_col_2_heading("meters");
+  print_col_3_heading("meters/s");
+  PRINT_NEW_LINE;
 
+  // used to keep track of what time we are at
   double current_hour;
   current_hour = initial_hour;
 
+  // used for recording the highest altitude
+  // and at what time that occurred
   double highest_alt, highest_alt_time;
   highest_alt = 0.0;
   highest_alt_time = 0.0;
 
+  // loop from the initial time to the
+  // final time, while doing the calculations
   while(current_hour <= final_hour){
     // print the current time in hours
-    print_d(&current_hour);
+    print_hour(current_hour);
 
     // calculate and print the altitude
     double altitude;
-    altitude = calculate_altitude(&current_hour);
-    print_d(&altitude);
+    altitude = calculate_altitude(current_hour);
+    print_altitude(altitude);
 
     // check for the highest altitude
     if (altitude > highest_alt){
@@ -127,11 +182,11 @@ int main(){
 
     // calculate and print the velocity
     double velocity;
-    velocity = calculate_velocity(&current_hour);
-    print_d(&velocity);
+    velocity = convert_mph_to_mps( calculate_velocity(current_hour) );
+    print_velocity(velocity);
 
-    // print a newline
-    printf(NEW_LINE);
+    // print a newline at the end of each row
+    PRINT_NEW_LINE;
 
     // move forward in time
     current_hour += increment_hour;
@@ -139,11 +194,11 @@ int main(){
 
   // space the maximum altitude display away
   // from the table
-  printf(NEW_LINE); printf(NEW_LINE);
+  PRINT_NEW_LINE; PRINT_NEW_LINE;
 
-  printf("Maximum ballon height was %.2f meters", highest_alt);
-  printf(NEW_LINE);
-  printf("That occurred at %.2f hours", highest_alt_time);
+  // print the maximum height
+  printf("Maximum ballon height was %.2f meters", highest_alt); PRINT_NEW_LINE;
+  printf("That occurred at %.2f hours", highest_alt_time); PRINT_NEW_LINE;
 
   return 0;
 }
